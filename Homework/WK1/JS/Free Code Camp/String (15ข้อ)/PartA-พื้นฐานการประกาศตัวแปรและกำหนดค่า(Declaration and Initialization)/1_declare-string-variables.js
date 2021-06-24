04babe9d8b64e8c2d325087548d66012dda72039
Chanwit21 <bom.suthi@gmail.com>
2021-06-24 19:14:24 +0700

    Home Work WK1

---
+++ Homework/WK1/JS/Free Code Camp/String (15ข้อ)/PartA-พื้นฐานการประกาศตัวแปรและกำหนดค่า(Declaration and Initialization)/1_declare-string-variables.js
@@ -0,0 +1,2 @@
+var myFirstName = "Chanwit";
+var myLastName = 'Pansila';
---
+++ Homework/WK1/JS/Free Code Camp/String (15ข้อ)/PartB-การเชื่อมข้อความ(String_Concatenate)/3_constructing-strings-with-variables.js
@@ -0,0 +1,3 @@
+// Only change code below this line
+var myName = "Chanwit";
+var myStr = "My name is " + myName + "and I am well!" ;
