43f0bf09c02c0b52e835b544baf577691f8abb84
Chanwit21 <bom.suthi@gmail.com>
2021-06-27 17:22:12 +0700

    edit homework lab loop

--- JS/Homework/JavaScriptWeek2/CC_JSPart1_Loop  5ข้อ  Lab ข้อ 6-10/Lab8/Javascript.js
+++ JS/Homework/JavaScriptWeek2/CC_JSPart1_Loop  5ข้อ  Lab ข้อ 6-10/Lab8/Javascript.js
@@ -1,7 +1,8 @@
 console.log("ให้เขียนโค้ดเพื่อรับ input มาเป็นตัวเลข โดยให้รับมาเรื่อยๆ จนกว่า ผู้ใช้จะพิมพ์ 0, เลขลบ, string ว่าง, กด cancel หรือ esc จึงหยุดรับตัวเล")
 console.log("ให้หาผลรวมและค่าเฉลี่ยของเลขที่ผู้ใช้งานกรอกเข้ามา")
 
-let result = 0;
+let sum = 0;
+let count = 0;
 let input = 0;
 //แบบที่1
 while (true) {
@@ -9,8 +10,9 @@ while (true) {
     if (input < 0 ||input ==="0" || input === null || input.trim() === "" || !(+input)) { //!(+input) ใช้ Check ว่าเป็น String ไหม
         break;
     };
-    result += +input;
+    count++;
+    sum += +input;
     // console.log(input);
 };
 
-alert(`ผลรวมของค่าทั้งหมดที่ใส่เข้ามาคือ ${result}`);
+alert(`ผลรวมของค่าทั้งหมดที่ใส่เข้ามาคือ ${sum} ผลรวมของค่าทั้งหมดที่ใส่เข้ามาคือ ${sum/count}`);
