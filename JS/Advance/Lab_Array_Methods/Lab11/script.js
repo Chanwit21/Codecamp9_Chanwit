c1c98c7616de13e6c86a220a803276408af7d84d
Chanwit21 <bom.suthi@gmail.com>
2021-07-01 18:39:47 +0700

    Array Methode

---
+++ JS/Advance/Lab_Array_Methods/Lab11/script.js
@@ -0,0 +1,5 @@
+// จงหาผลรวมของ element ใน array โดยใช้ forEach
+const array = [29, 17, 13, 47, 23, 31];
+let sum = 0;
+array.forEach(item => sum += item)
+console.log(sum);
---
+++ JS/Advance/Lab_Array_Methods/Lab12/script.js
@@ -0,0 +1,9 @@
+// ให้ใช้ forEach และ console.log ให้ได้ผลลัพธ์แบบใน comment ด้านล่าง
+const names = ['Mike', 'Sid', 'Jack', 'Bill'];
+// 1. Mike
+// 2. Sid
+// 3. Jack
+// 4. Bill
+names.forEach((element,index) => {
+    console.log(`${index+1}. ${element}`);    
+});
