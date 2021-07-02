aea1ccf3da47ad3524689a4b48fa866a4a060432
Chanwit21 <bom.suthi@gmail.com>
2021-07-02 19:52:37 +0700

    Edit Lab Array Methode 40

--- JS/Advance/Lab_Array_Methods/Lab40/script.js
+++ JS/Advance/Lab_Array_Methods/Lab40/script.js
@@ -1,5 +1,5 @@
 // จงเขียนฟังก์ชัน isArray(input) เพื่อตรวจสอบว่า input เป็น Array หรือไม่ 
-// function isArray(input){
-//     return Array.isArray(input);
-// }
+function isArray(input){
+    return Array.isArray(input);
+}
 console.log(isArray([]));
