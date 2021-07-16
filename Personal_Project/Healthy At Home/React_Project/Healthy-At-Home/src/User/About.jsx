cea018df3c1dc14c47a9395fae25f6f7f58052c6
Chanwit21 <bom.suthi@gmail.com>
2021-07-16 19:22:52 +0700

    Finished Figma In Personal Project And Create React To Project

---
+++ Personal_Project/Healthy At Home/React_Project/Healthy-At-Home/src/User/About.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import Nav from "./Nav";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./Home";
+
+function About() {
+  return (
+      <div>
+        <p>NAV PAge</p>
+      </div>
+  );
+}
+
+export default About;
