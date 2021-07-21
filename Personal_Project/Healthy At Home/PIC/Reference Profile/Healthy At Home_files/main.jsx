bb39a4354b90e68ca02b90fcbc65df8ca211a103
Chanwit21 <bom.suthi@gmail.com>
2021-07-21 11:42:33 +0700

    Add PesonalProject

---
+++ Personal_Project/Healthy At Home/PIC/Reference Profile/Healthy At Home_files/main.jsx
@@ -0,0 +1,7 @@
+import __vite__cjsImport0_react from "/node_modules/.vite/react.js?v=2d867caa"; const React = __vite__cjsImport0_react.__esModule ? __vite__cjsImport0_react.default : __vite__cjsImport0_react;
+import __vite__cjsImport1_reactDom from "/node_modules/.vite/react-dom.js?v=2d867caa"; const ReactDOM = __vite__cjsImport1_reactDom.__esModule ? __vite__cjsImport1_reactDom.default : __vite__cjsImport1_reactDom;
+import "/src/index.css";
+import App from "/src/App.jsx?t=1626792225481";
+ReactDOM.render(/* @__PURE__ */ React.createElement(React.StrictMode, null, /* @__PURE__ */ React.createElement(App, null)), document.getElementById("root"));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIkQ6L0NvZGUvUGVyc29uYWxfUHJvamVjdF9DQzkvSGVhdGh5QXRIb21lLWZyb250LWVuZC9zcmMvbWFpbi5qc3giXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFJlYWN0IGZyb20gJ3JlYWN0J1xuaW1wb3J0IFJlYWN0RE9NIGZyb20gJ3JlYWN0LWRvbSdcbmltcG9ydCAnLi9pbmRleC5jc3MnXG5pbXBvcnQgQXBwIGZyb20gJy4vQXBwJ1xuXG5SZWFjdERPTS5yZW5kZXIoXG4gIDxSZWFjdC5TdHJpY3RNb2RlPlxuICAgIDxBcHAgLz5cbiAgPC9SZWFjdC5TdHJpY3RNb2RlPixcbiAgZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoJ3Jvb3QnKVxuKVxuIl0sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUVBLFNBQVMsT0FDUCxvQ0FBQyxNQUFNLFlBQVAsTUFDRSxvQ0FBQyxLQUFELFFBRUYsU0FBUyxlQUFlOyIsIm5hbWVzIjpbXX0=
