79ff52a5fe0b41edb914fded4b4cd53033744a0d
Chanwit21 <bom.suthi@gmail.com>
2021-07-29 09:11:43 +0700

    Edit test

--- Extera_Class14Day/Class/test.js
+++ Extera_Class14Day/Class/test.js
@@ -1,14 +1,32 @@
 // const x = 0;
-const y = 1;
-const z = 2;
-// if( x != y)console.log('Hi')
-// console.log('Hello')
-// let b = 0;
-// const a = (x===y)?"Hello":(z > y)? "Hi": 'last';
-// console.log(a)
-// console.log(b)
-let x = 1
-while (x < 7) {
-    console.log(x)
-    x++
+// const y = 1;
+// const z = 2;
+// // if( x != y)console.log('Hi')
+// // console.log('Hello')
+// // let b = 0;
+// // const a = (x===y)?"Hello":(z > y)? "Hi": 'last';
+// // console.log(a)
+// // console.log(b)
+// let x = 1
+// while (x < 7) {
+//     console.log(x)
+//     x++
+// }
+
+class Test {
+    constructor(_num1,_num2){
+        this.num1 = _num1;
+        this.num2 = _num2;
+    }
+    static testMethode() {
+        return this.reduce((acc,crr,index) => {
+            if(index === 0) return crr;
+            if(crr>acc) return acc;
+            return acc; 
+        },0);
+    }
 }
+
+const test = new Test(1,2);
+console.log(test)
+console.log(test.testMethode())
--- JS/Advance/Lab_Prototype/Lab1/script.js
+++ JS/Advance/Lab_Prototype/Lab1/script.js
@@ -23,5 +23,24 @@ Array.prototype.max = function () {
         return accumulator;
     },0);
 };
-console.log([1,2,3,4,500,6,7,8,9,10,100].max())
+[1,2,3,4,500,6,7,8,9,10,100].max()
 console.log([10,20,30,40,52,45,354,4,513,].max())
+
+class Test {
+    constructor(arr){
+        this.ar = arr;
+    }
+    
+    max(arr) {
+        
+    }
+}
+
+Test.prototype.max = function () {
+    return this.reduce((accumulator,currentvalue,index) => {
+        if (index === 0) return currentvalue;
+        if (currentvalue > accumulator) return currentvalue;
+        return accumulator;
+    },0);
+};
+[1,2,3,4,500,6,7,8,9,10,100].max()
